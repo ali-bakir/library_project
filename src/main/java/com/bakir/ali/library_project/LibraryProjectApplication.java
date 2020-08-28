43f6159430adac6c04431ba85774a359de63208d
alibakr <alibakrpeyzaj@gmail.com>
2020-08-28 14:26:26 +0300

    add missing main file

---
+++ src/main/java/com/bakir/ali/library_project/LibraryProjectApplication.java
@@ -0,0 +1,10 @@
+package com.bakir.ali.library_project;
+
+import org.springframework.boot.SpringApplication;
+
+public class LibraryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryProjectApplication.class, args);
+    }
+}
