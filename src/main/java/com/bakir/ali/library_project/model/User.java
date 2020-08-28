ba7ecdbd49165ce92f6644253bfb7b251dcf0bf6
alibakr <alibakrpeyzaj@gmail.com>
2020-08-28 14:25:28 +0300

    implement user model

---
+++ src/main/java/com/bakir/ali/library_project/model/User.java
@@ -0,0 +1,39 @@
+package com.bakir.ali.library_project.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
