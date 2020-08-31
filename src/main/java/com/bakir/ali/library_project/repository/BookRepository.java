766909a0fc43d0eef681430e8cddfcb100b93b60
alibakr <alibakrpeyzaj@gmail.com>
2020-08-31 23:50:01 +0300

    The bookreposity file has been edited and added.

---
+++ src/main/java/com/bakir/ali/library_project/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.bakir.ali.library_project.repository;
+
+import com.bakir.ali.library_project.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> {
+}
