a916d3fcc2d74c21138750d06a6442ba2a68b6f5
alibakr <alibakrpeyzaj@gmail.com>
2020-08-31 23:49:45 +0300

    The authorreposity file has been edited and added.

---
+++ src/main/java/com/bakir/ali/library_project/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.bakir.ali.library_project.repository;
+
+import com.bakir.ali.library_project.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author,Long> {
+}
