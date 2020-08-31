8ea682cc233a996faf2f6de048cd651eb10d56f0
alibakr <alibakrpeyzaj@gmail.com>
2020-08-31 23:50:14 +0300

    The publisherreposity file has been edited and added.

---
+++ src/main/java/com/bakir/ali/library_project/repository/PublisherRepository.java
@@ -0,0 +1,9 @@
+package com.bakir.ali.library_project.repository;
+
+import com.bakir.ali.library_project.model.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher,Long> {
+}
