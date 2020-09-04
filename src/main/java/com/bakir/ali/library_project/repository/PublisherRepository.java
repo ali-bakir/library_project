7e9948c309493b796e49814e4f0eaeb1dacca702
alibakr <alibakrpeyzaj@gmail.com>
2020-09-05 02:32:48 +0300

    fix format

--- src/main/java/com/bakir/ali/library_project/repository/PublisherRepository.java
+++ src/main/java/com/bakir/ali/library_project/repository/PublisherRepository.java
@@ -2,8 +2,10 @@ package com.bakir.ali.library_project.repository;
 
 import com.bakir.ali.library_project.model.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PublisherRepository extends JpaRepository<Publisher,Long> {
+public interface PublisherRepository extends JpaRepository<Publisher, Long>, JpaSpecificationExecutor<Publisher> {
+
 }
