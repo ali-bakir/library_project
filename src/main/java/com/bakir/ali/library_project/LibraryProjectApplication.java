61f4534edcdeb591640b2523400e7a91d3dcd2dd
alibakr <alibakrpeyzaj@gmail.com>
2020-09-08 01:51:32 +0300

    edited and classes, html files included.

--- src/main/java/com/bakir/ali/library_project/LibraryProjectApplication.java
+++ src/main/java/com/bakir/ali/library_project/LibraryProjectApplication.java
@@ -1,10 +1,17 @@
 package com.bakir.ali.library_project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController("/")
 public class LibraryProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryProjectApplication.class, args);
     }
+    @GetMapping ("/index")
+    public String index () { return "login.html";}
 }
