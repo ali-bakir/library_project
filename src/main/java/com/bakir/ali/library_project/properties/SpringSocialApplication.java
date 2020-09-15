f555d1780701629970bc0550c288a5eb8c594364
alibakr <alibakrpeyzaj@gmail.com>
2020-09-15 23:29:18 +0300

    the file has been created and edited.

---
+++ src/main/java/com/bakir/ali/library_project/properties/SpringSocialApplication.java
@@ -0,0 +1,14 @@
+package com.bakir.ali.library_project.properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
+public class SpringSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSocialApplication.class, args);
+    }
+}
