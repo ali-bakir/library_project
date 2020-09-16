c3b3fba95ffe852348b07f1409c174a29d6656a0
alibakr <alibakrpeyzaj@gmail.com>
2020-09-16 23:13:24 +0300

    userrepository file has been edited.

--- src/main/java/com/bakir/ali/library_project/repository/UserRepository.java
+++ src/main/java/com/bakir/ali/library_project/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package com.bakir.ali.library_project.repository;
 
-import com.bakir.ali.library_project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
 
 }
