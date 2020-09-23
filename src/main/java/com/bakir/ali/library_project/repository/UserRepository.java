a2c522a1a9d3af207c6d7c8dc79a17a257df1cb9
alibakr <alibakrpeyzaj@gmail.com>
2020-09-23 17:14:45 +0300

    the file has been reorganized.

--- src/main/java/com/bakir/ali/library_project/repository/UserRepository.java
+++ src/main/java/com/bakir/ali/library_project/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.bakir.ali.library_project.repository;
 
+import com.bakir.ali.library_project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
