79e71380962eb74a352dafe4f17cd5cc14562e73
alibakr <alibakrpeyzaj@gmail.com>
2020-09-23 16:22:06 +0300

    editing has been done and errors have been fixed.

---
+++ src/main/java/com/bakir/ali/library_project/factory/OAuth2UserInfoFactory.java
@@ -0,0 +1,11 @@
+package com.bakir.ali.library_project.factory;
+
+import com.bakir.ali.library_project.oauth2.user.OAuth2UserInfo;
+
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+    public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+        return null;
+    }
+}
