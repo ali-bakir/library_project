002d3102b9c94694cebedb8e2ce3ba0d34249ee8
alibakr <alibakrpeyzaj@gmail.com>
2020-10-06 02:08:05 +0300

    The new file was created and the content was written.

---
+++ src/main/java/com/bakir/ali/library_project/security/CurrentUser.java
@@ -0,0 +1,12 @@
+package com.bakir.ali.library_project.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
