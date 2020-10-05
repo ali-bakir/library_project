698c420188456182f62efd3875211668efcf11bd
alibakr <alibakrpeyzaj@gmail.com>
2020-10-06 02:00:31 +0300

    the file has been created and edited.

---
+++ src/main/java/com/bakir/ali/library_project/oauth2/user/OAuth2UserInfoFactory.java
@@ -0,0 +1,19 @@
+package com.bakir.ali.library_project.oauth2.user;
+
+import com.bakir.ali.library_project.exception.OAuth2AuthenticationProcessingException;
+import com.bakir.ali.library_project.model.AuthProvider;
+
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+
+    public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+        if(registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
+            return new GoogleOAuth2UserInfo(attributes);
+        } else if (registrationId.equalsIgnoreCase(AuthProvider.github.toString())) {
+            return new GithubOAuth2UserInfo(attributes);
+        } else {
+            throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
+        }
+    }
+}
