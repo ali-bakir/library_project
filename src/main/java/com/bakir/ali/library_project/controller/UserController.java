9751067d86f863e76c8047aca5a52de001bdc056
alibakr <alibakrpeyzaj@gmail.com>
2020-10-06 01:20:26 +0300

    the locations of some files have been changed. a new arrangement was made.

--- src/main/java/com/bakir/ali/library_project/controller/UserController.java
+++ src/main/java/com/bakir/ali/library_project/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.bakir.ali.library_project.controller;
 
+import com.bakir.ali.library_project.exception.ResourceNotFoundException;
 import com.bakir.ali.library_project.model.User;
+import com.bakir.ali.library_project.security.CurrentUser;
+import com.bakir.ali.library_project.security.UserPrincipal;
 import com.bakir.ali.library_project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import static com.bakir.ali.library_project.repository.UserRepository.userRepository;
 
 @RestController
 @RequestMapping (value = "/users", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -24,4 +30,12 @@ public class UserController {
         return ResponseEntity.ok(userService.saveUser(user));
     }
 
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+        return userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    }
 }
+
+
