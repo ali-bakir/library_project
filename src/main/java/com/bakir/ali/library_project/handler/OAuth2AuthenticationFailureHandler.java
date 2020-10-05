bd78e01ff866f7929e2d3b3b110ea77a7f034704
alibakr <alibakrpeyzaj@gmail.com>
2020-10-06 01:56:21 +0300

    the file has been edited. additions were made.

--- src/main/java/com/bakir/ali/library_project/handler/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/bakir/ali/library_project/handler/OAuth2AuthenticationFailureHandler.java
@@ -1,21 +1,40 @@
 package com.bakir.ali.library_project.handler;
 
+import com.bakir.ali.library_project.repository.HttpCookieOAuth2AuthorizationRequestRepository;
+import com.bakir.ali.library_project.utils.CookieUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class OAuth2AuthenticationFailureHandler implements AuthenticationFailureHandler {
+import static com.bakir.ali.library_project.repository.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
+@Component
+public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Autowired
-    private OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+    HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
 
     @Override
-    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String targetUrl;
+        targetUrl = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map((Object t) -> Cookie.getValue(t))
+                .orElse(("/"));
 
+        targetUrl = UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("error", exception.getLocalizedMessage())
+                .build().toUriString();
+
+        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 }
